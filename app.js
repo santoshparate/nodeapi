1f88bea26e0a197f7934a5a76c79a6d42ae86046
santoshparate <santoshparate2001@gmail.com>
2021-02-02 13:45:59 +0530

    Express module add created get and post API

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
-const express = require('express') ;
+const express = require('express');
 
 const app = express();
+// Get API to read data and send as response 
+app.get('',(req,res)=>{
+
+    res.send('<h1>Hello I am first page</h2>');
+
+})
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>About Us</h1>')
+})
+
+app.get('/team',(req,res)=>{
+res.send('<h1>Team</h1>')
+})
+
+// Post API to receive user data via form or post API
+app.post('/adduser',(req,res)=>{
+
+
+    console.log(req.body);
+    res.send('Data Saved Successfully!!!')
+})
+
+// console.log(__dirname);
+// console.log(__filename);
+// Send JSON object as response 
+app.get('/students', (req, res) => {
+
+    res.send([
+        {name:'Santosh'},
+        {name:'Pankaj'},
+        {name:'Shailendra'}
+    ])
+})
+
+
+app.listen(8000, () => {
+    console.log('Server is up running at port 8000')
+})
